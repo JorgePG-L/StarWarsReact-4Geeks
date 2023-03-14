1894e0c4a094f99f83ec9aa79d6f22af783fc3f9
Miquel Ortiz Graupera <sisasfx@gmail.com>
2023-03-14 22:35:58 +0000

    feat:card done

--- src/js/services/service.js
+++ src/js/services/service.js
@@ -1,9 +1,14 @@
 const BASE_URL = "https://www.swapi.tech/api"
 
-async function getPeople() {
+export async function getPeople() {
     const response = await fetch(BASE_URL+"/people", {method:"GET"})
     const dataResponse = await response.json();
-    return dataResponse
+    return dataResponse;
 }
 
-export default getPeople;
+export async function getInfoPeople(){
+    const response = await fetch(BASE_URL+"/people/1", {method:"GET"})
+    const dataResponse = await response.json();
+    return dataResponse;
+}
+
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,26 +1,42 @@
 import React,{useState, useEffect} from "react";
-import getPeople from "../services/service";
-import rigoImage from "../../img/rigo-baby.jpg";
+import {getPeople, getInfoPeople} from "../services/service.js";
 import "../../styles/home.css";
-
+import soldados from "../../img/soldadosImperio.jpg"
+import darthVader from "../../img/darthVader.jpg"
 export const Home = () => {
 	const [state, setState] = useState([])
+	const [state2, setState2] = useState("");
 
 	async function getDataFromFunction(){
 		const dataFromFetch = await getPeople()
-		console.log(dataFromFetch.results)
 		setState(dataFromFetch.results)		
+	}
+
+	async function getSingleData(){
+			const dataFromFetch = await getInfoPeople()
+			console.log(dataFromFetch)
+			setState2(dataFromFetch.result.description)
 	}
 
 	useEffect(() => {
 		getDataFromFunction()
+		getSingleData()
 	},[])
 
 
 	return(
-		<div className="text-center mt-5">
+		<div className="container mt-5">
 			{
-				state.map((element, key) => <h5>Hello {element.name}</h5>)
+				state.map((element, key) => 
+				<div className="card">
+					 <img class="card-img-top" src={key % 2 != 0 ? soldados : darthVader} alt="Card image"></img>
+					<h5 className="card-title">{element.name}</h5>
+					<p className="card-text">{state2}</p>
+					<div className="card-footer">
+						<button className="btn btn-success">More Info</button>
+						<button className="btn btn-warning">Add to Chart</button>			
+					</div>
+				</div>)
 			}
 		</div>
 	)
